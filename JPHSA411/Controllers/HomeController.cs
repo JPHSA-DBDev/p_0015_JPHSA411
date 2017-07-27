52cc1acf609e96325c50ee48a236e0aa083f5c44
ggarson <ggarson@WIN10VM>
2017-07-27 11:08:09 -0500

    Add project files.

---
+++ JPHSA411/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JPHSA411.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult IT_Dept()
+        //{
+        //    //ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult SysAid_Incident()
+        //{
+
+        //    return View();
+        //}
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
